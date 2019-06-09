b09eacf147b92829c3efb90a65d135607077a7f1
bridget <breekiki03@gmail.com>
2019-06-09 20:08:13 +0300

    Update the js

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $(".image1").click(function() {
+      $(".p1").toggle();
+     
+    });
+  });
+    
+  $(document).ready(function() {
+    $(".image2").click(function() {
+      $(".p2").toggle();
+     
+    });
+  });
+  $(document).ready(function() {
+    $(".image3").click(function() {
+      $(".p3").toggle();
+     
+    });
+  });
