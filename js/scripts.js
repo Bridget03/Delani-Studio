189fbc58ce2b157a327abad8057e0642b84f3d93
bridget <breekiki03@gmail.com>
2019-06-10 12:14:51 +0300

    Add the interactivity

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
 
     });
 });
-
 $(document).ready(function () {
     $(".image2").click(function () {
         $(".p2").toggle();
@@ -19,3 +18,34 @@ $(document).ready(function () {
 
     });
 });
+$(document).ready(function () {
+    $('.text').hide();
+    $('.image').animate({
+        opacity: 1
+
+    });
+
+    $('.image').hover(function () {
+        $(this).stop().animate({ opacity: .4 }, 200);
+        $('.text').fadeIn();
+
+    }, function () {
+        $(this).stop().animate({ opacity: 1 }, 500)
+        $('.text').fadeOut();
+    });
+
+
+    $("#mannu").submit(function (event) {
+        var val1 = (document.getElementById("name").value);
+        var val2 = (document.getElementById("email").value);
+        var val3 = (document.getElementById("textarea").value);
+
+        if (!val1 || !val2 || !val3) {
+            alert("Fill in the form!!!");
+        }
+        else {
+            alert("Thank you for your feeback!!!");
+        }
+    });
+});
+
