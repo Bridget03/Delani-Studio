43671c56e729e1dd4c9d341d99d35c80c4ff2c7f
bridget <breekiki03@gmail.com>
2019-06-10 09:02:17 +0300

    Add the toggling efect

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
-$(document).ready(function() {
-    $(".image1").click(function() {
-      $(".p1").toggle();
-     
+$(document).ready(function () {
+    $(".whatwedo p").hide();
+
+    $(".image1").click(function () {
+        $(".p1").toggle();
+
     });
-  });
-    
-  $(document).ready(function() {
-    $(".image2").click(function() {
-      $(".p2").toggle();
-     
+});
+
+$(document).ready(function () {
+    $(".image2").click(function () {
+        $(".p2").toggle();
+
     });
-  });
-  $(document).ready(function() {
-    $(".image3").click(function() {
-      $(".p3").toggle();
-     
+});
+$(document).ready(function () {
+    $(".image3").click(function () {
+        $(".p3").toggle();
+
     });
-  });
+});
